d8fac3e73758bee03e5f740a0c8c33d4d9f13bb1
abdeladim101basraoui <abdoobas101@gmail.com>
2022-07-13 15:31:25 +0100

    refresh token access for other update

---
+++ DiabetesOnContainer/Models/RefreshToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiabetesOnContainer.Models
+{
+    public partial class RefreshToken
+    {
+        public Guid Id { get; set; }
+        public string Role { get; set; } = null!;
+        public string Token { get; set; } = null!;
+        public DateTime Created { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
