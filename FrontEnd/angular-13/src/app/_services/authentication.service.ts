e58965d119affdbbe99ae2560cacbbcf16e12f62
b1sra0u1 <abdoobas101@gmail.com>
2022-07-30 13:09:33 +0100

    getting the date and updating the role claims in backend

--- FrontEnd/angular-13/src/app/_services/authentication.service.ts
+++ FrontEnd/angular-13/src/app/_services/authentication.service.ts
@@ -1,30 +1,40 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { loginUser, } from '../_models/LoginUser_model';
-import { Route } from '@angular/compiler/src/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { tap } from 'rxjs/operators';
-import { userRole } from '../_models/userRole_model';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { loginUser } from "../_models/LoginUser_model";
+import { Route } from "@angular/compiler/src/core";
+import { Router } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
+import { environment } from "src/environments/environment";
+import { tap } from "rxjs/operators";
+import { userRole } from "../_models/userRole_model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthenticationService {
-  private readonly patientUrl = 'FichePatients';
+  //call Url
+  private readonly patientUrl = "FichePatients";
+  
+  //the payload infos
   user!: userRole;
 
-  constructor(private http: HttpClient, private routing: Router) {
-    //TODO in here we should check the expired date  and refresh the token
+
+  constructor(private http: HttpClient, private routing: Router) 
+  {
+    //TODO in here we should check the expired date and refresh the token
     this._isLoggedIn.next(!!this.Token);
 
     // this._isSuperUser.next(!!this.user.role.includes('Doc'));
-    // console.log(new Date(this.user.exp*1000));
-    
-    
+    console.log(`the token is ${this.Token}`);
+
     //the state of the token
     this.user = this.getUser(this.Token!);
+    
+    console.log(`the date is ${
+      new Date(this.user.exp*1000)
+    }`);
+ console.log(`the role is ${this.user.role}`);
+ 
   }
 
   // -->describe the state of the user  -- default state is faulse
@@ -37,44 +47,44 @@ export class AuthenticationService {
   public isLoggedIn = this._isLoggedIn.asObservable();
   public isSuperUser = this._isSuperUser.asObservable();
 
-
   get Token() {
-    return localStorage.key(0)?.toString();
+    return localStorage.getItem(localStorage.key(0)?.toString()!);
   }
 
-  public tokenName: string = '';
+  public tokenName: string = "";
   ServerLogin(credentials: loginUser) {
-
-    let link: string = '';
+    let link: string = "";
     if (credentials.role == "Doc") {
       link = `${environment.baseAPIUrl}/admin/Diabeticiens/login`;
-      this.tokenName = 'JWT Doc'
+      this.tokenName = "JWT Doc";
     }
 
     if (credentials.role == "Assist") {
       link = `${environment.baseAPIUrl}/admin/Assistants/login`;
-      this.tokenName = 'JWT Assist'
+      this.tokenName = "JWT Assist";
     }
 
-    return this.http.post(link, { 'email': credentials.email, "password": credentials.password }, { responseType: 'text' }).pipe(
-      tap((response: any) => {
+    return this.http
+      .post(
+        link,
+        { email: credentials.email, password: credentials.password },
+        { responseType: "text" }
+      )
+      .pipe(
+        tap((response: any) => {
+          //save the token to local storage
+          localStorage.setItem(this.tokenName, response);
 
-        //save the token to local storage
-        localStorage.setItem(this.tokenName, response);
-
-        this.user= this.getUser(response);
-        //keep the state of the user
-        this._isLoggedIn.next(true);
-
-      })
-    );
-
+          this.user = this.getUser(response);
+          //keep the state of the user
+          this._isLoggedIn.next(true);
+        })
+      );
   }
-
 
   //
   Register(data: any, role: string) {
-    let link: string = '';
+    let link: string = "";
 
     if (role == "Doc") {
       link = `${environment.baseAPIUrl}/admin/Diabeticiens/register`;
@@ -84,27 +94,24 @@ export class AuthenticationService {
       link = `${environment.baseAPIUrl}/admin/Assistants/Register`;
     }
 
-
     const token = localStorage.key(0)?.toString();
     // const httpheader = new HttpHeaders({
     //   'Content-Type': 'application/json',
     //   'Authorization': `Bearer ${token}`
     // })
 
-
-    this.http.post(link, data /*,{ headers: httpheader }*/)
-      .subscribe(response => {
-        this.routing.navigate(['']);
-
-      }, err => {
+    this.http.post(link, data /*,{ headers: httpheader }*/).subscribe(
+      (response) => {
+        this.routing.navigate([""]);
+      },
+      (err) => {
         console.log(err);
-      });
-
+      }
+    );
   }
 
-
   RoleCanAccess(accessmanu: any) {
-    const accessRole = localStorage.getItem('JWT Doc');
+    const accessRole = localStorage.getItem("JWT Doc");
 
     if (accessRole) {
       return true;
@@ -113,15 +120,12 @@ export class AuthenticationService {
     return false;
   }
 
-  getFichePatient() {
+  getFichePatient() {}
 
-  }
-
-//get the data from the token {role <==> exp date}
+  //get the data from the token {role <==> exp date}
   private getUser(token: string): userRole {
     // return JSON.parse(atob(token.split('.')[1])) as userRole;
     // this._isSuperUser.next(!!this.user.role.includes('Doc'));
-    return JSON.parse(atob(token.split('.')[1])) ;
+    return JSON.parse(atob(token.split(".")[1]));
   }
 }
-
