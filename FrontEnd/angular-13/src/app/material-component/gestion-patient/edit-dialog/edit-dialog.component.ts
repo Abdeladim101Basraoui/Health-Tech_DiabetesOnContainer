f7c0dad0bc3529581547ece2a635367cb6b84210
abdeladim101basraoui <abdoobas101@gmail.com>
2022-08-01 08:50:40 +0100

    adding momentjs

--- FrontEnd/angular-13/src/app/material-component/gestion-patient/edit-dialog/edit-dialog.component.ts
+++ FrontEnd/angular-13/src/app/material-component/gestion-patient/edit-dialog/edit-dialog.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import {default as _rollupMoment} from 'moment';
+
 
 @Component({
   selector: 'app-edit-dialog',
   templateUrl: './edit-dialog.component.html',
-  styleUrls: ['./edit-dialog.component.css']
+  styleUrls: ['./edit-dialog.component.css'],
+ providers: [
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
 export class EditDialogComponent implements OnInit {
 
@@ -26,7 +39,8 @@ export class EditDialogComponent implements OnInit {
           cin:this.cin,
           nom:this.nom,
           prenom:this.nom,
-          date:this.datenaissance,
+          // date: this.datenaissance.value.toISOString().split('T')[0],// this.datenaissance,
+          date:  this.datenaissance,
           sexe:this.gendervalidate
         }
       )
